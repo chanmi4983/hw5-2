17fc4ec73164e27610de5039e2bd1ea7f598eb79
nachanmi <chanmi4983@naver.com>
2024-11-15 21:06:35 +0900

    finish

--- src/components/BookCarousel.js
+++ src/components/BookCarousel.js
@@ -7,36 +7,36 @@ const BookCarousel = () => {
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://covers.openlibrary.org/b/id/8228691-L.jpg"
+                    src="https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9791162890936.jpg"
                     alt="First slide"
                     style={{ height: '400px', objectFit: 'cover' }}
                 />
                 <Carousel.Caption>
-                    <h3>To Kill a Mockingbird</h3>
-                    <p>Harper Lee's classic novel of courage and morality.</p>
+                    <h3>이달의 베스트 셀러</h3>
+                    <p>한동대학교 김광 교수님의 문제부터 풀자! C Programming</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://covers.openlibrary.org/b/id/7222246-L.jpg"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSge9CgoZXCF808oyBmL_SSQ2sxkn6FeGxSpw&s"
                     alt="Second slide"
                     style={{ height: '400px', objectFit: 'cover' }}
                 />
                 <Carousel.Caption>
-                    <h3>1984</h3>
-                    <p>George Orwell's dystopian masterpiece.</p>
+                    <h3>한국 최초 노벨문학상</h3>
+                    <p>한강의 채식 주의자</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://covers.openlibrary.org/b/id/7355045-L.jpg"
+                    src="https://i.namu.wiki/i/bDgDXYacwX0K5YpC8ku3DeA_7_ZJmg45Shp_m4iCBjSWHHXl3rfyoL4bkArNba4hnAKz1IflkfXe7_fmVuhW8A.webp"
                     alt="Third slide"
                     style={{ height: '400px', objectFit: 'cover' }}
                 />
                 <Carousel.Caption>
-                    <h3>The Great Gatsby</h3>
+                    <h3>비극적인 사랑, The Great Gatsby</h3>
                     <p>F. Scott Fitzgerald's story of the Jazz Age.</p>
                 </Carousel.Caption>
             </Carousel.Item>
